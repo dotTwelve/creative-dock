9ae0821eb42cd0c3189d41e841ad497cb239fb74
Jan Machač <ext.Jan.Machac@skoda-auto.cz>
2021-05-06 11:23:35 +0200

    slider

--- ms-direct/index.js
+++ ms-direct/index.js
@@ -1,1 +1,15 @@
-console.log("ne");
+var Webflow = Webflow || [];
+Webflow.push(function () {
+
+    $("#orders-slider").slider({
+        value: 100,
+        min: 0,
+        max: 500,
+        step: 50,
+        slide: function (event, ui) {
+            $("#orders-slider-input").val("$" + ui.value);
+        }
+    });
+    $("#orders-slider-input").val("$" + $("#orders-slider").slider("value"));
+
+});
