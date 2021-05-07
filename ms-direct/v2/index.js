4ed5b35d7d6c4ab7cb7f711a704f52a19aa8cf37
Jan Machač <ext.Jan.Machac@skoda-auto.cz>
2021-05-07 12:54:21 +0200

    v2

---
+++ ms-direct/v2/index.js
@@ -0,0 +1,44 @@
+var Webflow = Webflow || [];
+Webflow.push(function () {
+
+    if ($('#survey').length > 0) {
+        $('form :input').on('change input', function () {
+            $(window).bind('beforeunload', function () {
+                return 'Are you sure you want to leave?';
+            });
+        });
+    }
+
+    var inputOrder = $('#orders-slider-input');
+    var inputPick = $('#picks-slider-input');
+    var sliderOrder = $('#orders-slider');
+    var sliderPick = $('#picks-slider');
+
+    inputOrder.prop('readonly', true).val(200);
+    inputPick.prop('readonly', true).val(5);
+
+    sliderOrder.slider({
+        range: "min",
+        value: 200,
+        min: 1,
+        max: 1000,
+        step: 1,
+        slide: function (event, ui) {
+            inputOrder.val(ui.value);
+        }
+    });
+    inputOrder.val(sliderOrder.slider("value"));
+
+    sliderPick.slider({
+        range: "min",
+        value: 5,
+        min: 1,
+        max: 12,
+        step: 1,
+        slide: function (event, ui) {
+            inputPick.val(ui.value);
+        }
+    });
+    inputPick.val(sliderPick.slider("value"));
+
+});
