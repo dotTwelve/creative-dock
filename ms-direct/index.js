f804f15946ce9833281abed1be79b233212ac013
Jan Machač <ext.Jan.Machac@skoda-auto.cz>
2021-05-07 08:50:03 +0200

    exit message

--- ms-direct/index.js
+++ ms-direct/index.js
@@ -2,8 +2,10 @@ var Webflow = Webflow || [];
 Webflow.push(function () {
 
     if ($('#survey').length > 0) {
-        $(window).bind('beforeunload', function () {
-            return 'Are you sure you want to leave?';
+        $('form :input').on('change input', function () {
+            $(window).bind('beforeunload', function () {
+                return 'Are you sure you want to leave?';
+            });
         });
     }
 
