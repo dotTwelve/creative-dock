90bba7b13e32a76a651a4e50aea47bafcd303541
Jan Machač <ext.Jan.Machac@skoda-auto.cz>
2021-05-25 17:33:35 +0200

    form update

--- ms-direct/v2/index.js
+++ ms-direct/v2/index.js
@@ -1,13 +1,17 @@
 var Webflow = Webflow || [];
 Webflow.push(function () {
 
-    /*if ($('#survey').length > 0) {
+    if ($('#survey').length > 0) {
         $('form :input').on('change input', function () {
 
             $(window).bind('beforeunload', function () {
                 return 'Are you sure you want to leave?';
             });
         });
-    }*/
+
+        $('form').submit(function () {
+            $(window).unbind('beforeunload');
+        });
+    }
 
 });
